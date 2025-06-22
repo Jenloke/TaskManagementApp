93a0e00b44846e2ec9ce36ebc7a978465c69e18d
Jenloke <mjenloke@gmail.com>
2025-06-22 14:59:39 +0800

    basic add and read && layout for delete, complete and update

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import os
+from datetime import datetime 
+
 class Task:
   def __init__(self, task_ID, title, description, due_date, priority_level, status, creation_timestamp):
     self.task_ID = task_ID
@@ -8,23 +11,63 @@ class Task:
     self.status = status
     self.creation_timestamp = creation_timestamp
 
-task = []
+task_list = []
 
-def add_task():
-  pass
+def add_task(task):
+  task_list.append(Task(**task))
+  print(task_list[0].task_ID)
 
 def read_task():
-  pass
+  # should read from db
+  for task in task_list:
+    print(task.task_ID)  
 
 def update_task():
+  # select ID
+    # check if id is correct - return if error or proceed if correct
+    # retrieve task details from db
+  # update selected attribute
+  # return success
   pass
 
 def complete_task():
+  # select ID
+    # check if id is correct - return if error or proceed if correct
+    # retrieve task details from db
+  # update selected attribute
+  # return success
   pass
 
 def delete_task():
+  # select ID
+    # check if id is correct - return if error or proceed if correct
+    # retrieve task details from db
+  # update selected attribute
+  # return success
   pass
 
 # Main Program Loop
-while True:
-  pass
+# print("""
+#       Task Management Application
+#       [1] Add task
+#       [2] Read All tasks
+#       [3] Update a task's details
+#       [4] Mark a task as complete
+#       [5] Delete a task
+#       """)
+# x = input("Select an option: ")
+# os.system('cls') # Clear current console input per operation
+
+
+# logic after operation call 
+add_task({
+  'task_ID': 0, 
+  'title': "Make bed", 
+  'description': "Do everyday", 
+  'due_date': datetime.now(), 
+  'priority_level': 1, 
+  'status': 1, 
+  'creation_timestamp': datetime.now()
+})
+
+read_task()
