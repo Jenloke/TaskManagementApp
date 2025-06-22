e7b010d9ec6958bebd7af6e6c6eeccb8d4c02d60
Jenloke <mjenloke@gmail.com>
2025-06-22 14:25:37 +0800

    __init__

---
+++ main.py
@@ -0,0 +1,15 @@
+class Task:
+  def __init__(self, task_ID, title, description, due_date, priority_level, status, creation_timestamp):
+    self.task_ID = task_ID
+    self.title = title
+    self.description = description
+    self.due_date = due_date
+    self.priority_level = priority_level
+    self.status = status
+    self.creation_timestamp = creation_timestamp
+
+task = {
+  
+}
+
+new = Task(...)
