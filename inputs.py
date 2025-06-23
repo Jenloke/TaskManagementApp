e91928543d1342c02396b90c2df8699a6108e0a9
Jenloke <mjenloke@gmail.com>
2025-06-23 20:46:46 +0800

    new format and add find id from tasks_collections

--- inputs.py
+++ inputs.py
@@ -21,23 +21,32 @@ def get_int_input_in_range(prompt, min_value, max_value):
 
 def get_date_input(prompt, formats=None, must_be_future=False):
   if formats is None:
-    formats = ["%m-%d-%Y"]  # default format
+    formats = ["%m-%d-%Y"]  # default date format: MM-DD-YYYY
 
   while True:
     date_str = input(prompt).strip()
     for fmt in formats:
       try:
         date_obj = datetime.strptime(date_str, fmt)
-
-        # Optional future/past validation
         now = datetime.now()
-        if must_be_future and date_obj >= now:
-          print("Date must be in the future.")
-          break
-        
-        return date_obj.replace(tzinfo=timezone.utc)  # valid date found
+        if must_be_future and date_obj.date() < now.date():
+          print("Date must be today or in the future.")
+          break 
+
+        return date_obj.replace(tzinfo=timezone.utc)
+
       except ValueError:
         continue
 
     else:
       print(f"Invalid date format. Supported formats: {', '.join(formats)}")
+      
+def find_id_input(tasks_collection):
+  while True:
+    input_id = input("Enter Task ID: ")
+    if_found = tasks_collection.find_one({'task_id': int(input_id)})
+    
+    if if_found:
+      return int(input_id)
+    else:
+      print('Task ID not found. Please try again.')
